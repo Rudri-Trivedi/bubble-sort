60181763a15bf04eb059fe99e8f059b22a71ee8a
Rudri Trivedi <rudritrivedi28@gmail.com>
2024-08-09 18:50:55 +0530

    add user input code

--- bubble.py
+++ bubble.py
@@ -1,5 +1,18 @@
 a = []
 
+while True: 
+        print('\nadd - adding value.')
+        print('q - recive shorted value.')
+
+        user_input= input()
+
+        if user_input == 'add':
+            element = input('Entar a value. ')
+            a.append(element)
+
+        if user_input == 'q':
+             break
+
 for i in range(len(a)):
     for j in range(i+1,len(a)):
         if int(a[i]) >int(a[j]) :
